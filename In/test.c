e74cd454c5c06ecc23fd90f9685391460d731fcc
Erfan <2001erfan1380@gmsil.com>
2021-11-03 00:24:51 +0330

    add regex for operation-comparison-punctuation & get library name

---
+++ In/test.c
@@ -0,0 +1,11 @@
+#include "mylib.txt"
+#define start int main()
+int main(){
+int a=3;
+float b=5;
+int c=7;
+int d=a+c;
+for(int i=0;i<3;i++){
+}
+
+}
--- dfa2.py
+++ dfa2.py
@@ -1,47 +1,80 @@
 import re
-import string
-with open('test.c') as f:
+# inputFile = input("Enter Input Path\n")
+# outputFile = input("Enter Output Path\n")
+with open("In/test.c") as f:
     lines = f.readlines()
 
 output: list = []
-
+operationRegex = "[\+|\-|\*|\/|\%]+[\=]|[\+]+[\+]|[\-]+[\-]|[\+|\-|\*|\/|\%]|[\=]"
+comparisonRegex = "[\<|\>|\=|\!]+[\=]|[\<]|[\>]"
+punctuationRegex = "\(|\)|\;|\,|\[|\]|\{|\}"
 keyword = ["if", "else", "main", "while", "int",
            "float", "for", "string", "do", "#include"]
-punctuations = ["(", ")", ";", ",", "[", "]", "{", "}"]
-comparison = [">", "<", "<=", "==", ">=", "!="]
-# operations = ["+", "-", "*", "/", "%", "=",
-#               "++", "--", "+=", "-=", "/=", "*=", "%="]
-operations = ["+", "-", "*", "/", "%", "="]
+
+
+def finder(arr: list):
+    return [x for x in arr if x]
 
 
 def dfa(str1: str):
-    state = 'a'
     begin = 0
-    str1 = str1.replace(" ", "")
+    if(str1.startswith("#include")):
+        output.append(("library", str1.split(" ")[1][1:-1]))
+        return
+    if(str1.startswith("#define")):
+        output.append(("definition", " ".join(str1.split(" ")[1:])))
+        return
+    # str1 = str1.replace(" ", "")
     flag = False
     for i in range(len(str1)):
-        if(re.findall("^[a-zA-Z_0-9]*", str1[i])[0]):
+        if(finder(re.findall("^[a-zA-Z_0-9]*", str1[i]))):
             continue
         else:
             token = str1[begin:i]
             if(token in keyword):
                 output.append(("keyword", token))
-            elif(re.findall("^[0-9]*", token)[0]):
+            elif(finder(re.findall("^[0-9]*", token))):
                 output.append(("number", token))
-            elif(re.findall("^[a-zA-Z_0-9]*", token)[0]):
+            elif(finder(re.findall("^[a-zA-Z_0-9]*", token))):
                 output.append(("identifier", token))
-            if(str1[i] in punctuations):
+            if(finder(re.findall(punctuationRegex, str1[i]))):
                 output.append(("punctuation", str1[i]))
-            if(str1[i] in comparison):
-                output.append(("comparison", str1[i]))
-            if(str1[i] in operations and str1[i+1] == "="):
-                output.append(("operation", str1[i]+str1[i+1]))
-                flag = True
-            elif(str1[i] in operations and not(flag)):
-                output.append(("operation", str1[i]))
+            if(i <= len(str1)-2 and finder(re.findall(comparisonRegex, str1[i]+str1[i+1]))):
+                if(not(flag) and len(re.findall(comparisonRegex, str1[i]+str1[i+1])[0]) == 2):
+                    output.append(("comparison", str1[i]+str1[i+1]))
+                    flag = True
+                elif(not(flag) and len(re.findall(comparisonRegex, str1[i]+str1[i+1])[0]) == 1):
+                    output.append(("operation", str1[i]))
+                else:
+                    flag = False
+            if(i <= len(str1)-2 and finder(re.findall(operationRegex, str1[i]+str1[i+1]))):
+                if(not(flag) and len(re.findall(operationRegex, str1[i]+str1[i+1])[0]) == 2):
+                    output.append(("operation", str1[i]+str1[i+1]))
+                    flag = True
+                elif(not(flag) and len(re.findall(operationRegex, str1[i]+str1[i+1])[0]) == 1):
+                    output.append(("operation", str1[i]))
+                else:
+                    flag = False
             begin = i+1
 
 
+isComment = False
+
 for i in lines:
-    dfa(i.strip())
-print(output)
+    i = i.strip()
+    # Delete Comments
+    if i.endswith("*/"):
+        isComment = False
+    if isComment:
+        isComment = False
+        continue
+    if not i.startswith("//") and not i.startswith("/*"):
+        dfa(i.strip())
+    elif i.startswith("/*"):
+        isComment = True
+with open("out/answer.txt", "w") as f:
+    for i in output:
+        f.write(i[0])
+        f.write(":: ")
+        f.write(i[1])
+        f.write("\n")
