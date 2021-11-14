28cb801ab9a9f388dd209ae468ac955f288a2893
Erfan <2001erfan1380@gmsil.com>
2021-11-14 20:38:06 +0330

    fix problem

--- dfa2.py
+++ dfa2.py
@@ -66,38 +66,38 @@ def replaceDefines(data: str):
     return "".join(res)
 
 
-# def errorHandling(data: str):
-#     # Default Value should be 1 for "int main()"
-#     loopsCount = 1
-#     parenthesesCount = 0
-#     # Loops Count
-#     for item in ["for", "while", "if", "else"]:
-#         loopsCount += data.count(item)
-#     # Parentheses Count
-#     for item in ["{", "}"]:
-#         parenthesesCount += data.count(item)
-#     res = data.split("\n")
-#     for index in range(len(res)):
-#         # Parentheses Checking in while and for loop
-#         if(res[index].startswith("for") or res[index].startswith("while")):
-#             if(not(finder(re.findall("\)", res[index]) and finder(re.findall("\(", res[index]))))):
-#                 print("In Line %d Has Error!!!!" % index)
-#         # Semicolon Checking
-#         if finder(re.findall("^int|float|string|char", res[index])):
-#             if(res[index][-1] != ";" and not finder(re.findall("main", res[index]))):
-#                 print("In Line %d Has Error!!!!" % index)
-#          # ParenthesesCount in forLoop Checking
-#         if finder(re.findall("^for", res[index])):
-#             if(res[index].count(";") != 2):
-#                 print("In Line %d Has Error!!!!" % index)
-#         # ParenthesesCount Checking
-#         if(loopsCount*2 != parenthesesCount):
-#             print("ParenthesesCount Error!!!!")
-#         # Check File Exist
-#         if(res[index].startswith("#include")):
-#             fileName = res[index].split(" ")[1:]
-#             if(not os.path.isfile(fileName)):
-#                 print("In Line %d Has Error!!!!" % index)
+def errorHandling(data: str):
+    # Default Value should be 1 for "int main()"
+    loopsCount = 1
+    parenthesesCount = 0
+    # Loops Count
+    for item in ["for", "while", "if", "else"]:
+        loopsCount += data.count(item)
+    # Parentheses Count
+    for item in ["{", "}"]:
+        parenthesesCount += data.count(item)
+    res = data.split("\n")
+    for index in range(len(res)):
+        # Parentheses Checking in while and for loop
+        if(res[index].startswith("for") or res[index].startswith("while")):
+            if(not(finder(re.findall("\)", res[index]) and finder(re.findall("\(", res[index]))))):
+                print("In Line %d Has Error!!!!" % index)
+        # Semicolon Checking
+        if finder(re.findall("^int|float|string|char", res[index])):
+            if(res[index][-1] != ";" and not finder(re.findall("main", res[index]))):
+                print("In Line %d Has Error!!!!" % index)
+         # ParenthesesCount in forLoop Checking
+        if finder(re.findall("^for", res[index])):
+            if(res[index].count(";") != 2):
+                print("In Line %d Has Error!!!!" % index)
+        # ParenthesesCount Checking
+        if(loopsCount*2 != parenthesesCount):
+            print("ParenthesesCount Error!!!!")
+        # Check File Exist
+        if(res[index].startswith("#include")):
+            fileName = res[index].split(" ")[1:]
+            if(not os.path.isfile(fileName)):
+                print("In Line %d Has Error!!!!" % index)
 
 
 # Read FileData from Source File
@@ -112,12 +112,14 @@ result = replaceIncludes(program)
 result2 = replaceDefines(result)
 # Calling remove Spaces
 result3 = removeSpaces(result2)
+# Calling Error Handling
+result4 = errorHandling(result3)
 # Write In New File
 with open("In/new.txt", "w") as f:
     f.write(result3)
 
 # Operation Regex
-operationRegex = "[\+|\-|\*|\/|\%]+[\=]|[\+][\+]|[\-][\-]|[\+|\-|\*|\/|\%]|[\=]"
+operationRegex = "[\+|\-|\*|\/|\%]+[\=]|[\+]+[\+]|[\-]+[\-]|[\+|\-|\*|\/|\%]|[\=]"
 # Comparison Regex
 comparisonRegex = "[\<|\>|\=|\!]+[\=]|[\<]|[\>]"
 # Punctuation Regex
@@ -127,47 +129,44 @@ keyword = ["if", "else", "main", "while", "int",
            "float", "for", "string", "do", "#include"]
 
 output: list = []
-operationArray = ["+", "-", "*", "/", "%", "="]
-comparisonArray = ["<", ">", "!"]
 
 
-def dfa(str1: str , lineNumber : int ):
+def dfa(str1: str, lineNumber: int):
     begin = 0
     # str1 = str1.replace(" ", "")
-    i = 0
-    while(i < len(str1)):
-        if(not finder(re.findall("^[a-zA-Z_0-9]*", str1[i]))):
+    flag = False
+    for i in range(len(str1)):
+        if(finder(re.findall("^[a-zA-Z_0-9]*", str1[i]))):
+            continue
+        else:
             token = str1[begin:i]
-            if(finder(re.findall("^[0-9]+[a-zA-Z]+", token))):
-                print("Line %d has lexical Error (identifier)" % index)
             if(token in keyword):
                 output.append(("keyword", token))
             elif(finder(re.findall("^[0-9]+[a-zA-Z_0-9]+", token))):
-                print("lexical error that happened in line : %d "%lineNumber)
+                print("lexical error that happened in line : %d " % lineNumber)
             elif(finder(re.findall("^[0-9]*", token))):
                 output.append(("number", token))
-            elif(finder(re.findall("^[_a-zA-Z]+[a-zA-Z_0-9]*", token))):
+            elif(finder(re.findall("^[a-zA-Z_0-9]*", token))):
                 output.append(("identifier", token))
             if(finder(re.findall(punctuationRegex, str1[i]))):
                 output.append(("punctuation", str1[i]))
-            if(str1[i] in comparisonArray):
-                currentIndex = i
-                while (str1[i] in comparisonArray):
-                    i += 1
-                if(len(finder(re.findall(comparisonRegex, str1[currentIndex:i]))) == 1):
-                    output.append(("comparison", str1[currentIndex:i]))
+            if(i <= len(str1)-2 and finder(re.findall(comparisonRegex, str1[i]+str1[i+1]))):
+                if(not(flag) and len(re.findall(comparisonRegex, str1[i]+str1[i+1])[0]) == 2):
+                    output.append(("comparison", str1[i]+str1[i+1]))
+                    flag = True
+                elif(not(flag) and len(re.findall(comparisonRegex, str1[i]+str1[i+1])[0]) == 1):
+                    output.append(("operation", str1[i]))
                 else:
-                    print("Line %d has lexical Error (comparison)" % index)
-            if(str1[i] in operationArray):
-                currentIndex = i
-                while (str1[i] in operationArray):
-                    i += 1
-                if(len(finder(re.findall(operationRegex, str1[currentIndex:i]))) == 1):
-                    output.append(("operation", str1[currentIndex:i]))
+                    flag = False
+            if(i <= len(str1)-2 and finder(re.findall(operationRegex, str1[i]+str1[i+1]))):
+                if(not(flag) and len(re.findall(operationRegex, str1[i]+str1[i+1])[0]) == 2):
+                    output.append(("operation", str1[i]+str1[i+1]))
+                    flag = True
+                elif(not(flag) and len(re.findall(operationRegex, str1[i]+str1[i+1])[0]) == 1):
+                    output.append(("operation", str1[i]))
                 else:
-                    print("Line %d has lexical Error (operation)" % index)
+                    flag = False
             begin = i+1
-        i += 1
 
 
 # Read Lines from Source File
@@ -175,8 +174,8 @@ with open("In/new.txt") as f:
     lines = f.readlines()
 lineNumber = 1
 for i in lines:
-    dfa(i ,lineNumber)
-    lineNumber +=1
+    dfa(i, lineNumber)
+    lineNumber += 1
 # Write in Output File
 with open("out/answer.txt", "w") as f:
     for i in output:
